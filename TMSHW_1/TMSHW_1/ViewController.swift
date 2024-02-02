c1146664de044bb9c53b5f8152bebc701de83473
evilgen <foxik1985@mail.ru>
2024-02-02 21:46:18 +0300

    update 0.2
    Add Button/Text/switch

--- TMSHW_1/TMSHW_1/ViewController.swift
+++ TMSHW_1/TMSHW_1/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var label1   : UILabel!
+    @IBOutlet weak var switchB1 : UISwitch!
+    @IBOutlet weak var switchB2 : UISwitch!
+    @IBOutlet weak var switchB3 : UISwitch!
+    @IBOutlet weak var switchB4 : UISwitch!
+    @IBOutlet weak var switchB5 : UISwitch!
+    
+    @IBOutlet weak var Textview1   : UITextView!
+    
+    @IBAction func button1(_ sender: UIButton) {
+        
+        label1.text = "Yuhoo...2"
+               switchB1.tintColor = .red
+               switchB1.isOn = false
+        Textview1.textColor = .blue
+        Textview1.text = "многотекстамноготекстамноготекстамноготекстамноготекстамноготекстамноготекстамноготекста многотекстамноготекстамноготекста многотекстамноготекстамноготекста"
+    }
+    
 }
 
